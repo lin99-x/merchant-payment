1ae9c2b67bbfd3490ac989503e15cf378be58111
gaojinglin99 <gaojinglin449@gmail.com>
2026-03-19 13:43:54 +0100

    Add JWT configurations

--- sky-common/src/main/java/com/sky/constant/JwtClaimsConstant.java
+++ sky-common/src/main/java/com/sky/constant/JwtClaimsConstant.java
@@ -2,9 +2,8 @@ package com.sky.constant;
 
 public class JwtClaimsConstant {
 
-    public static final String EMP_ID = "empId";
-    public static final String USER_ID = "userId";
-    public static final String PHONE = "phone";
+    public static final String USER_ID = "id";
+    public static final String MERCHANT_ID = "merchantId";
     public static final String USERNAME = "username";
     public static final String NAME = "name";
 
--- sky-common/src/main/java/com/sky/utils/JwtUtil.java
+++ sky-common/src/main/java/com/sky/utils/JwtUtil.java
@@ -10,47 +10,44 @@ import java.util.Map;
 
 public class JwtUtil {
     /**
-     * 生成jwt
-     * 使用Hs256算法, 私匙使用固定秘钥
+     * Generate JWT token
+     * Use HS256 algorithm to sign the token, and set the expiration time and claims
      *
-     * @param secretKey jwt秘钥
-     * @param ttlMillis jwt过期时间(毫秒)
-     * @param claims    设置的信息
+     * @param secretKey JWT secret key
+     * @param ttlMillis JWT expiration time in milliseconds
+     * @param claims    JWT claims
      * @return
      */
     public static String createJWT(String secretKey, long ttlMillis, Map<String, Object> claims) {
-        // 指定签名的时候使用的签名算法，也就是header那部分
+        // Algorithm for signing the JWT, using the HS256 algorithm and the provided secret key
         SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 
-        // 生成JWT的时间
+        // Calculate the expiration time of the JWT, which is the current time plus the provided ttlMillis
         long expMillis = System.currentTimeMillis() + ttlMillis;
         Date exp = new Date(expMillis);
 
-        // 设置jwt的body
+        // Build the JWT token using the Jwts.builder() method
         JwtBuilder builder = Jwts.builder()
-                // 如果有私有声明，一定要先设置这个自己创建的私有的声明，这个是给builder的claim赋值，一旦写在标准的声明赋值之后，就是覆盖了那些标准的声明的
                 .setClaims(claims)
-                // 设置签名使用的签名算法和签名使用的秘钥
                 .signWith(signatureAlgorithm, secretKey.getBytes(StandardCharsets.UTF_8))
-                // 设置过期时间
                 .setExpiration(exp);
 
         return builder.compact();
     }
 
     /**
-     * Token解密
+     * Parse JWT token
      *
-     * @param secretKey jwt秘钥 此秘钥一定要保留好在服务端, 不能暴露出去, 否则sign就可以被伪造, 如果对接多个客户端建议改造成多个
-     * @param token     加密后的token
+     * @param secretKey JWT secret key
+     * @param token     JWT token to be parsed
      * @return
      */
     public static Claims parseJWT(String secretKey, String token) {
-        // 得到DefaultJwtParser
+        // Parse the JWT token using the Jwts.parser() method, setting the signing key and parsing the token
         Claims claims = Jwts.parser()
-                // 设置签名的秘钥
+                // Set the signing key for verifying the JWT token, using the provided secret key
                 .setSigningKey(secretKey.getBytes(StandardCharsets.UTF_8))
-                // 设置需要解析的jwt
+                // Parse the JWT token and get the claims from the token
                 .parseClaimsJws(token).getBody();
         return claims;
     }
