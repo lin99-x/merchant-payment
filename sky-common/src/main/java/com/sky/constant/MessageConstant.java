cd5680af95619701f394b21c39efe105776afc39
gaojinglin99 <gaojinglin449@gmail.com>
2026-04-01 15:19:33 +0200

    Add new message constants

--- sky-common/src/main/java/com/sky/constant/MessageConstant.java
+++ sky-common/src/main/java/com/sky/constant/MessageConstant.java
@@ -1,27 +1,33 @@
 package com.sky.constant;
 
 /**
- * 信息提示常量类
+ * Error message constants
  */
 public class MessageConstant {
-
-    public static final String PASSWORD_ERROR = "密码错误";
-    public static final String ACCOUNT_NOT_FOUND = "账号不存在";
-    public static final String ACCOUNT_LOCKED = "账号被锁定";
-    public static final String UNKNOWN_ERROR = "未知错误";
-    public static final String USER_NOT_LOGIN = "用户未登录";
-    public static final String CATEGORY_BE_RELATED_BY_SETMEAL = "当前分类关联了套餐,不能删除";
-    public static final String CATEGORY_BE_RELATED_BY_DISH = "当前分类关联了菜品,不能删除";
-    public static final String SHOPPING_CART_IS_NULL = "购物车数据为空，不能下单";
-    public static final String ADDRESS_BOOK_IS_NULL = "用户地址为空，不能下单";
-    public static final String LOGIN_FAILED = "登录失败";
-    public static final String UPLOAD_FAILED = "文件上传失败";
-    public static final String SETMEAL_ENABLE_FAILED = "套餐内包含未启售菜品，无法启售";
-    public static final String PASSWORD_EDIT_FAILED = "密码修改失败";
-    public static final String DISH_ON_SALE = "起售中的菜品不能删除";
-    public static final String SETMEAL_ON_SALE = "起售中的套餐不能删除";
-    public static final String DISH_BE_RELATED_BY_SETMEAL = "当前菜品关联了套餐,不能删除";
-    public static final String ORDER_STATUS_ERROR = "订单状态错误";
-    public static final String ORDER_NOT_FOUND = "订单不存在";
-
+    public static final String PASSWORD_ERROR = "Incorrect password";
+    public static final String ACCOUNT_NOT_FOUND = "Account not found";
+    public static final String ACCOUNT_LOCKED = "Account is locked";
+    public static final String UNKNOWN_ERROR = "Unknown error";
+    public static final String USER_NOT_LOGIN = "User not logged in";
+    public static final String CATEGORY_BE_RELATED_BY_SETMEAL = "Category is associated with setmeal, cannot delete";
+    public static final String CATEGORY_BE_RELATED_BY_DISH = "Category is associated with dish, cannot delete";
+    public static final String SHOPPING_CART_IS_NULL = "Shopping cart is empty, cannot place order";
+    public static final String ADDRESS_BOOK_IS_NULL = "User address is empty, cannot place order";
+    public static final String LOGIN_FAILED = "Login failed";
+    public static final String UPLOAD_FAILED = "File upload failed";
+    public static final String SETMEAL_ENABLE_FAILED = "Setmeal contains unsold dishes, cannot enable";
+    public static final String PASSWORD_EDIT_FAILED = "Password modification failed";
+    public static final String DISH_ON_SALE = "Dishes on sale cannot be deleted";
+    public static final String SETMEAL_ON_SALE = "Setmeals on sale cannot be deleted";
+    public static final String DISH_BE_RELATED_BY_SETMEAL = "Dish is associated with setmeal, cannot delete";
+    public static final String ORDER_STATUS_ERROR = "Order status error";
+    public static final String ORDER_NOT_FOUND = "Order not found";
+    public static final String ORDER_CANNOT_CHECKOUT = "Order cannot be checked out";
+    public static final String PAYMENT_NOT_FOUND = "Payment not found";
+    public static final String PAYMENT_CANNOT_BE_REFUNDED = "Payment cannot be refunded";
+    public static final String REFUND_AMOUNT_EXCEEDS_PAYMENT_AMOUNT = "Refund amount exceeds payment amount";
+    public static final String PAYMENT_NOT_BELONG_TO_CURRENT_MERCHANT = "Payment does not belong to current merchant";
+    public static final String REFUND_NOT_FOUND = "Refund not found";
+    public static final String ONLY_PENDING_REFUND_CAN_BE_CANCELED = "Only pending refunds can be canceled";
+    public static final String ORDER_CANNOT_CANCEL = "Order cannot be canceled, can only cancel pending orders";
 }
